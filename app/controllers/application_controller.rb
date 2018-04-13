9cf0bd4ee4f93ae40e93ed91de78bcda20977c11
miriam <miriam5000@gmail.com>
2018-04-13 14:46:34 -0400

    application controller, add methods, sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,31 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+#enable sessions
+  enable :sessions
+  set :session_secret, "action_secret" 	
+  use Rack::Flash
+
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
   get "/" do
-    erb :welcome
+  	session.clear
+  	@actions = Act.all
+    erb :'index'
   end
+
+  helpers do
+      def logged_in?
+        !!session[:user_id]
+      end
+
+      def current_user
+         User.find(session[:user_id])
+      end
+    end 
 
 end
