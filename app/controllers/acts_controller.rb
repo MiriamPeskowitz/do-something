5a58a5de955b39a7f962fb76218fd677e230fe86
miriam <miriam5000@gmail.com>
2018-04-13 15:03:18 -0400

    acts controller, add methods

--- app/controllers/acts_controller.rb
+++ app/controllers/acts_controller.rb
@@ -1,1 +1,98 @@
-acts_controller.rb
+
+class ActionsController < ApplicationController
+	
+
+	get '/actions' do
+		if logged_in?
+			@actions = current_user.actions
+			erb :'actions/index' 
+		else 
+			redirect to '/citizens/login'
+		end 
+	end 
+
+
+	get '/actions/new' do
+		if logged_in? 
+			erb :'actions/new'
+		else 
+			redirect to '/citizens/login'	
+		end 
+	end 
+	
+
+	post '/actions' do
+		if logged_in? 
+
+			if current_user && params[:description] != ""
+				# @citizen = Citizen.find_by(:username => params[:username])
+				@action = Action.create(:title => params[:title], :date => params[:date], :description => params[:description], :citizen_id => current_user.id)
+	
+				
+				redirect to "/actions/#{@action.id}"
+			
+			else 
+				redirect to '/actions/new'
+			end
+		else
+			redirect to '/citizens/login'
+		end
+	end 
+
+
+
+	get '/actions/:id' do
+		if !logged_in?
+			redirect to '/citizens/login'	
+		else
+			@action = Action.find_by(:id => params[:id])
+		
+			erb :'actions/show'
+
+		end 
+	end 
+
+
+	get '/actions/:id/edit' do
+		# possible: use slug? 
+		# @action = Action.find_by_slug(params[:slug])
+		# @action.update(params[:title], params[:date], params[:description])
+
+		@action= Action.find_by(:id => params[:id])
+		if logged_in? && @action
+			if current_user.id == @action.citizen_id
+				erb :'actions/edit'
+			else 
+				redirect to '/actions'
+			end 
+		else
+		redirect to '/citizens/login'
+		end 
+	end 
+
+
+	patch '/actions/:id' do
+		@action = Action.find_by_id(params[:id])
+		redirect to '/citizens/login' if !logged_in?
+		if params[:description] == "" || current_user.id != @action.citizen_id
+			redirect to "/actions/#{@action.id}/edit"
+		else
+		    @action.update(:description => params[:description])
+		    redirect to "/actions/#{@action.id}"
+		end 
+	end 
+
+
+	delete '/actions/:id' do
+		@action = Action.find_by_id(params[:id])
+		redirect to "/" if !logged_in?
+
+		if @action && @action.citizen_id == current_user.id
+			@action.delete 
+			redirect to '/'
+		end
+	end
+end
+
+
+
---
+++ app/notes.md
@@ -0,0 +1,29 @@
+
+1. run corneal DONE
+2. Config.ru DONE
+3. config/environment.rb -- all good DONE
+4. add flash gems to gemfile, bundle DONE
+5. What I want this app to do: 
+	User comes in, signs in/logs in, and gets to their special place 
+
+	user writes down title and description of what they've done that day that makes them feel like they're part of a community, a nation, whatever, soemthing larger than themselves. 
+		on this page, you can link to see all your actions: 
+
+	user saves that
+
+	user can view it, delete it
+
+	user can return many times, add things, and see all of them. 
+
+	On the user's page, they can write down 3 things that they hope to do -- but only 3. No good to get overwhelmed. Ie, a tiny todo/planner, with delete capacity. 
+
+	--each user's page is private
+
+	Home page -- you can see many of these things-- to get the effect of it. 
+
+6. Create user and acts  DONE
+	Later: create user_future_acts model, user has_many acts
+	user ??
+
+7. 
+
