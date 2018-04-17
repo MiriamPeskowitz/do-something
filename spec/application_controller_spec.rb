1e943427b5794adb12e3394ad05e3750a6f5a071
miriam <miriam5000@gmail.com>
2018-04-17 16:21:27 -0400

    experimenting with writing user tests

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -4,8 +4,10 @@ def app
   ApplicationController
 end
 
+Started creating tests -- it's hard! 
+
 describe ApplicationController do
-  it "responds with a welcome message" do
+  it "clears the session, if it exists" do
     get '/'
     expect(last_response.status).to eq(200)
     expect(last_response.body).to include("Welcome to the Sinatra Template!")
@@ -13,8 +15,59 @@ describe ApplicationController do
 end
 
 describe "GET '/' - Greeting Form" do
-	it "welcomes the user" do
+	it "renders the welcome page' do
 		visit '/'
 		expect(page.body).to include("Welcome!")
 	end
 end 
+
+
+describe Actions do
+  describe #... do 
+    before do
+      @thing = Thing.new(:date => "Monday", :title => "    ". :description =>  "had a great time") 
+      @user = User.new(:name = "", :email = "")
+    end 
+
+    describe "..." do 
+      it "should do X" do 
+        expect(@xxx). to be_complete
+      end 
+
+      describe "LLL" do 
+        it "has a title and a description" do 
+          expect(@thing.title and .... )
+        end
+      end 
+    end 
+
+    -- hard to figure out the tests 
+
+
+describe "the signin process"
+  before :each do 
+  end 
+  
+  it 'signs me in' do
+    visit '/users/signin' 
+    within(#session) do
+    fill_in "Email", with 'user@mail.com'
+    fill_in "Password", with: 'password'
+
+code from applicationcontroller -- 
+
+
+  get "/" do
+    session.clear
+    @things = Thing.all
+    erb :welcome
+  end
+
+  helpers do
+      def logged_in?
+        !!session[:user_id]
+      end
+
+      def current_user
+         User.find(session[:user_id])
+      end
