e1267c8a5a4935b56931660c83ac5d9c08598355
miriam <miriam5000@gmail.com>
2018-04-19 13:15:18 -0400

    added capitalize method in user.rb, and tested. Working.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ActiveRecord::Base
 	has_many :futures
 	has_secure_password
 	validates_presence_of :username, :email, :password
+
+	def capitalize(username)
+		username.capitalize
+	end 
+	#can also modify in place (with exclamation mark/bang) :username => params[:usernname].capitalize!
 end 
 # add more specific validations, say, of password size? 
