b127c7644efeffbb6999c8afd763314bc15fc440
miriam <miriam5000@gmail.com>
2018-04-19 12:59:38 -0400

    save before adding upcase method in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates_presence_of :username, :email, :password
 end 
-
-# add has_many future_acts, through acts
+# add more specific validations, say, of password size? 
