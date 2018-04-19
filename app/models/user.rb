86d42d589e79d7e4715d453bcf698bbd2e28fa11
miriam <miriam5000@gmail.com>
2018-04-19 15:32:30 -0400

    experimenting with diff types of password length validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ActiveRecord::Base
 	has_many :things
 	has_many :futures
 	has_secure_password
-	validates_presence_of :username, :email, :password
+	validates_presence_of :username, :email, :password {:length => (:minimum => 8)}
+	# validates password, :presence => true, :length => (:minimum => 8)
 
 	def capitalize(username)
 		username.capitalize
@@ -10,3 +11,9 @@ class User < ActiveRecord::Base
 	#can also modify in place (with exclamation mark/bang) :username => params[:usernname].capitalize!
 end 
 # add more specific validations, say, of password size? 
+
+
+# # other possibilities for password validation: 
+# validates_presence_of :password_digest
+
+# validates_length_of :password, minimum: 6, if: Proc.new { |user| user.password.present? }
